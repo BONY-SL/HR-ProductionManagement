f35480a9f9d310dce3a6ba91038a2d34990f8054
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-03-04 18:01:47 +0530

    create project

---
+++ luckySystem/src/main/java/com/example/luckySystem/LuckySystemBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.luckySystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckySystemBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuckySystemBackEndApplication.class, args);
+	}
+
+}
---
+++ luckySystem/src/test/java/com/example/luckySystem/LuckySystemBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.luckySystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckySystemBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
