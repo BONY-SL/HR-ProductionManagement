09f8a340a834a74ce135a4558919a85b9d7f97e5
GeethmaMunasinghe <123428378+GeethmaMunasinghe@users.noreply.github.com>
2024-04-28 18:03:45 +0530

    create User And Get All  Users

---
+++ luckySystem/src/main/java/com/example/luckySystem/controller/user/MailRequest.java
@@ -0,0 +1,22 @@
+package com.example.luckySystem.controller.user;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MailRequest {
+    @NotBlank(message = "Recipient email is required")
+    @Email(message = "Email should be valid")
+    private String to;
+
+    @NotBlank(message = "Subject is required")
+    @Size(max = 512, message = "Subject cannot be longer than 255 characters")
+    private String content;
+
+    @NotBlank(message = "Content is required")
+    private String subject;
+}
--- luckySystem/src/main/java/com/example/luckySystem/service/user/UserService.java
+++ luckySystem/src/main/java/com/example/luckySystem/service/user/UserService.java
@@ -1,7 +1,9 @@
 package com.example.luckySystem.service.user;
+import com.example.luckySystem.dto.agent.AgentDTO;
 import com.example.luckySystem.dto.user.CredentialsDto;
 import com.example.luckySystem.dto.user.SignUpDto;
 import com.example.luckySystem.dto.user.UserDto;
+import com.example.luckySystem.entity.Agent;
 import com.example.luckySystem.entity.Employee;
 import com.example.luckySystem.entity.User;
 import com.example.luckySystem.exceptions.AppException;
@@ -10,6 +12,7 @@ import com.example.luckySystem.repo.employee.EmployeeRepo;
 import com.example.luckySystem.repo.user.UserRepo;
 import com.example.luckySystem.service.employee.EmployeeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -18,7 +21,9 @@ import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import java.nio.CharBuffer;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -37,6 +42,9 @@ public class UserService {
 
     private final EmployeeService employeeService;
 
+    @Autowired
+    private EmailService emailService;
+
 
     public UserDto login(CredentialsDto credentialsDto) {
         User user = userRepository.findByUsername(credentialsDto.username())
@@ -54,6 +62,7 @@ public class UserService {
 
     public void register(SignUpDto userDto) {
 
+        System.out.println("2"+userDto);
         Optional<User> optionalUser = userRepository.findByUsername(userDto.username());
         Boolean emailExists = userRepository.existsByEmail(userDto.email());
 
@@ -84,7 +93,10 @@ public class UserService {
         user.setContact(userDto.contact());
         user.setEmail(userDto.email());
         userRepository.save(user);
+        System.out.println("save");
+        System.out.println(user);
     }
+
     public UserDto findByUsername(String username) {
         User user = userRepository.findByUsername(username)
                 .orElseThrow(() -> new AppException("Unknown user", HttpStatus.NOT_FOUND));
@@ -108,6 +120,16 @@ public class UserService {
     public void deleteUserDetails(Long id) {
         User user = userRepository.findById(id).orElseThrow(() -> new RuntimeException("Agent not found"));
         userRepository.save(user);
+    }
+
+    public List<UserDto> getallUsers() {
+        List<User> users = userRepository.findAll();
+        return users.stream().map(this::convertUserEntityToDTO).collect(Collectors.toList());
+    }
+
+    private UserDto convertUserEntityToDTO(User unit) {
+
+        return new UserDto(unit.getId(),unit.getUsername(),unit.getPassword(),unit.getEmail(),unit.getContact(),unit.getRoles(),unit.getEmployee().getEmployee_id(),null);
     }
 
 }
