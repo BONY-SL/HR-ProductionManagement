114b9bfd1d8c2ee2da0155994c86156c0993b5be
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-05-19 16:07:40 +0530

    Update AgentRepo.java

--- luckySystem/src/main/java/com/example/luckySystem/repo/agent/AgentRepo.java
+++ luckySystem/src/main/java/com/example/luckySystem/repo/agent/AgentRepo.java
@@ -1,12 +1,7 @@
 package com.example.luckySystem.repo.agent;
-
-
 import com.example.luckySystem.entity.Agent;
-import com.example.luckySystem.entity.GoodProductsForLoading;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-import java.util.Date;
 import java.util.List;
 
 @Repository
