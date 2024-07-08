4f0ba2a434e367b7384ceec336baa62c81a5032b
DILSHAN PATHIRAGE <143308037+BONY-SL@users.noreply.github.com>
2024-07-09 02:57:46 +0530

    update

--- luckySystem/src/main/java/com/example/luckySystem/repo/depAndsec/DepartmentRepo.java
+++ luckySystem/src/main/java/com/example/luckySystem/repo/depAndsec/DepartmentRepo.java
@@ -3,6 +3,7 @@ import com.example.luckySystem.dto.employee.DepartmentEmployeeCountDto;
 import com.example.luckySystem.entity.Department;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,5 +15,8 @@ public interface DepartmentRepo extends JpaRepository<Department, String> {
             "FROM Department d LEFT JOIN d.employees e " +
             "GROUP BY d.department_name")
     List<DepartmentEmployeeCountDto> getDepartmentEmployeeCounts();
+
+    @Query("SELECT d FROM Department d WHERE d.department_name = :departmentName")
+    Department  findDepartmentIdByDepartmentName(@Param("departmentName") String departmentName);
 }
 
--- luckySystem/src/main/java/com/example/luckySystem/repo/salary/MedicalRepo.java
+++ luckySystem/src/main/java/com/example/luckySystem/repo/salary/MedicalRepo.java
@@ -1,7 +1,5 @@
 package com.example.luckySystem.repo.salary;
 import com.example.luckySystem.entity.Employee;
-import com.example.luckySystem.entity.EmployeeLeave;
-import com.example.luckySystem.entity.EmployeeAttendance;
 import com.example.luckySystem.entity.EmployeeMedical;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
